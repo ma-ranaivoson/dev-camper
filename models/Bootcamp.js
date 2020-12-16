6fa69a9ca4c2d09ed772eb4529cb99cfa544c548
ma_rvs <rv.nirmalala@gmail.com>
2020-12-16 16:52:32 +0100

    Creating Virtuals and Populate Course and Bootcamp

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -2,104 +2,110 @@ const mongoose = require('mongoose');
 const slugify = require('slugify');
 const geocoder = require('../utils/geocoder');
 
-const BootcampSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'Please add a name'],
-        unique: true,
-        trim: true,
-        maxlength: [50, 'Name can not be more than 50 characters'],
-    },
-    slug: String,
-    description: {
-        type: String,
-        required: [true, 'Please add a description'],
-        maxlength: [500, 'Name can not be more than 500 characters'],
-    },
-    website: {
-        type: String,
-        match: [
-            /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi,
-            'Please use a valid URL with HTTP or HTTPS',
-        ],
-    },
-    phone: {
-        type: String,
-        maxlength: [20, 'Phone number can not be more than 20 characters'],
-    },
-    email: {
-        type: String,
-        match: [
-            /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-            'Please add a valid email address',
-        ],
-    },
-    address: {
-        type: String,
-        required: [true, 'Please add an address'],
-    },
-    location: {
-        type: {
-            type: String, // Don't do `{ location: { type: String } }`
-            enum: ['Point'], // 'location.type' must be 'Point'
-            //required: true,
+const BootcampSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, 'Please add a name'],
+            unique: true,
+            trim: true,
+            maxlength: [50, 'Name can not be more than 50 characters'],
         },
-        coordinates: {
-            type: [Number],
+        slug: String,
+        description: {
+            type: String,
+            required: [true, 'Please add a description'],
+            maxlength: [500, 'Name can not be more than 500 characters'],
+        },
+        website: {
+            type: String,
+            match: [
+                /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi,
+                'Please use a valid URL with HTTP or HTTPS',
+            ],
+        },
+        phone: {
+            type: String,
+            maxlength: [20, 'Phone number can not be more than 20 characters'],
+        },
+        email: {
+            type: String,
+            match: [
+                /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                'Please add a valid email address',
+            ],
+        },
+        address: {
+            type: String,
+            required: [true, 'Please add an address'],
+        },
+        location: {
+            type: {
+                type: String, // Don't do `{ location: { type: String } }`
+                enum: ['Point'], // 'location.type' must be 'Point'
+                //required: true,
+            },
+            coordinates: {
+                type: [Number],
+                required: true,
+                index: '2Dsphere',
+            },
+            formattedAddress: String,
+            street: String,
+            city: String,
+            state: String,
+            country: String,
+            zipcode: String,
+        },
+        careers: {
+            // Array of strings
+            type: [String],
             required: true,
-            index: '2Dsphere',
+            enum: [
+                'Web Development',
+                'Mobile Development',
+                'UI/UX',
+                'Data Science',
+                'Business',
+                'Other',
+            ],
         },
-        formattedAddress: String,
-        street: String,
-        city: String,
-        state: String,
-        country: String,
-        zipcode: String,
+        averageRating: {
+            type: Number,
+            minlength: [1, 'Rating must be at least 1'],
+            maxlength: [10, 'Rating cannot be more than 10'],
+        },
+        averageCost: Number,
+        photo: {
+            type: String,
+            default: 'no-photo.jpg',
+        },
+        housing: {
+            type: Boolean,
+            default: false,
+        },
+        jobAssistant: {
+            type: Boolean,
+            default: false,
+        },
+        jobGuarantee: {
+            type: Boolean,
+            default: false,
+        },
+        acceptGi: {
+            type: Boolean,
+            default: false,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
     },
-    careers: {
-        // Array of strings
-        type: [String],
-        required: true,
-        enum: [
-            'Web Development',
-            'Mobile Development',
-            'UI/UX',
-            'Data Science',
-            'Business',
-            'Other',
-        ],
-    },
-    averageRating: {
-        type: Number,
-        minlength: [1, 'Rating must be at least 1'],
-        maxlength: [10, 'Rating cannot be more than 10'],
-    },
-    averageCost: Number,
-    photo: {
-        type: String,
-        default: 'no-photo.jpg',
-    },
-    housing: {
-        type: Boolean,
-        default: false,
-    },
-    jobAssistant: {
-        type: Boolean,
-        default: false,
-    },
-    jobGuarantee: {
-        type: Boolean,
-        default: false,
-    },
-    acceptGi: {
-        type: Boolean,
-        default: false,
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
-});
+    {
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true },
+    }
+);
 
 // Create bootcamp slug for the name
 BootcampSchema.pre('save', function (next) {
@@ -108,24 +114,41 @@ BootcampSchema.pre('save', function (next) {
     next();
 });
 
-// Geocode & create location
-// BootcampSchema.pre('save', async function (next) {
-//     const loc = await geocoder.geocode(this.address);
+// Cascade delete courses when a bootcamp is deleted
+BootcampSchema.pre('remove', async function (next) {
+    console.log(`Courses being removed from bootcamp ${this._id}`);
+    await this.model('Course').deleteMany({ bootcamp: this._id });
+    next();
+});
 
-//     this.location = {
-//         type: 'Point',
-//         coordinates: [loc[0].longitude, loc[0].latitude],
-//         formattedAddress: loc[0].formattedAddress,
-//         street: loc[0].streetName,
-//         city: loc[0].city,
-//         state: loc[0].stateCode,
-//         zipcode: loc[0].zipcode,
-//         country: loc[0].countryCode,
-//     };
+// Reverse populate with virtuals
+BootcampSchema.virtual('courses', {
+    ref: 'Course',
+    localField: '_id',
+    foreignField: 'bootcamp',
+    justOne: false,
+});
 
-//     // Do not put address in DB
-//     this.address = undefined;
-//     next();
-// });
+/*
+    // Geocode & create location
+    // BootcampSchema.pre('save', async function (next) {
+    //     const loc = await geocoder.geocode(this.address);
+
+    //     this.location = {
+    //         type: 'Point',
+    //         coordinates: [loc[0].longitude, loc[0].latitude],
+    //         formattedAddress: loc[0].formattedAddress,
+    //         street: loc[0].streetName,
+    //         city: loc[0].city,
+    //         state: loc[0].stateCode,
+    //         zipcode: loc[0].zipcode,
+    //         country: loc[0].countryCode,
+    //     };
+
+    //     // Do not put address in DB
+    //     this.address = undefined;
+    //     next();
+    // });
+*/
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
